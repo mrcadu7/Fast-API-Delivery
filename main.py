5da6266fc5b8762011a23da6fa18218813275536
Cadú <139708523+mrcadu7@users.noreply.github.com>
2023-11-28 15:28:36 -0300

    facilitando o uso de token no swagger

--- main.py
+++ main.py
@@ -3,8 +3,37 @@ from auth_routes import auth_router
 from order_routes import order_router
 from async_fastapi_jwt_auth import AuthJWT
 from schemas import Settings
+import inspect, re
+from fastapi import FastAPI
+from fastapi.routing import APIRoute
+from fastapi.openapi.utils import get_openapi
 
-app = FastAPI()
+app=FastAPI()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Delivey",
+        version="Version 1.0.0",
+        description="Freelance API",
+        routes=app.routes,
+    )
+    openapi_schema["components"]["securitySchemes"] = {"bearerAuth": {
+        "type": "http",
+        "scheme": "bearer",
+        "bearerFormat": "JWT"
+    }}
+
+    openapi_schema["security"] = [{"bearerAuth": []}]
+
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 @AuthJWT.load_config
 def get_config():
@@ -12,4 +41,3 @@ def get_config():
 
 app.include_router(auth_router)
 app.include_router(order_router)
-
